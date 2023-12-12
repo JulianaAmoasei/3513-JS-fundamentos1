57a0a86bb01e2b7b9200b781e0f5dc8ad7480a7c
JulianaAmoasei <late.parrot@gmail.com>
2023-12-12 15:04:03 -0300

    video: truthy e falsy

--- operadores-logicos.js
+++ operadores-logicos.js
@@ -1,5 +1,6 @@
 const notaFinal = 6;
-const faltas = 5;
+const faltas = 2;
+const advertencias = 0;
 
 if (notaFinal < 7 && faltas > 4) {
   console.log('reprovado, boas festas');
@@ -7,3 +8,8 @@ if (notaFinal < 7 && faltas > 4) {
   console.log('não foi reprovado por falta');
 }
 
+if (faltas >= 2 && !advertencias) {
+  console.log('recebeu bônus');
+} else {
+  console.log('não recebeu bônus');
+}
