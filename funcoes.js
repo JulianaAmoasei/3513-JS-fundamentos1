0a14a7a0ee28c4feb16959cbf81490c44e8a19e1
JulianaAmoasei <late.parrot@gmail.com>
2023-12-12 16:46:00 -0300

    video: relembrando funções

---
+++ funcoes.js
@@ -0,0 +1,9 @@
+// parâmetros/argumentos
+// retorno
+
+function exibeInfosEstudante(nome, nota) {
+  return `o nome é ${nome} e a nota é ${nota}`;
+}
+
+console.log(exibeInfosEstudante('Caroline', 10));
+console.log(exibeInfosEstudante('Ana', 7));
