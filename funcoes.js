747f42f309e3a845fcdb7d3558c98641498713fd
JulianaAmoasei <late.parrot@gmail.com>
2023-12-12 16:59:30 -0300

    video: expressão de função

--- funcoes.js
+++ funcoes.js
@@ -1,9 +1,10 @@
 // parâmetros/argumentos
 // retorno
+console.log(exibeInfosEstudante('Caroline', 10));
+console.log(exibeInfosEstudante('Ana', 7));
 
 function exibeInfosEstudante(nome, nota) {
   return `o nome é ${nome} e a nota é ${nota}`;
 }
 
-console.log(exibeInfosEstudante('Caroline', 10));
-console.log(exibeInfosEstudante('Ana', 7));
+// hoisting
